d76baa6b8821fc8b86d5f7ab930861435e123b8e
Rizwana <160624420+RizwanaBasheer@users.noreply.github.com>
2024-04-15 08:37:11 +0100

    Create app.py
    Testing

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+
+from logging.config import dictConfig
+
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+})
+
+app = Flask(__name__)
+CORS(app)
+# My SQL Instance configurations
+# Change the HOST IP and Password to match your instance configurations
+
+@app.route("/test")#URL leading to method
+def test(): # Name of the method
+ return render_template('login.html') #indent this line
+
+
+
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0',port='8080')

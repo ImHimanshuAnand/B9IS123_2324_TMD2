c2c8e555c6f7e5c7fa4fabda25e13dbd253b3e47
Himanshu Anand <93127466+ImHimanshuAnand@users.noreply.github.com>
2024-04-18 14:51:04 +0000

    test db creation

--- init.sh
+++ init.sh
@@ -29,6 +29,13 @@ MYSQL_PASSWORD="hv7460"
 MYSQL_HOST="localhost"
 MYSQL_DATABASE="Library"
 
+# MySQL command to create database
+CREATE_DB_QUERY="CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
+
+# Execute Mysql command, -e means next arg will be a sql query, "$CREATE_DB_QUERY" will be replaced by value from SHELL Variable.
+sudo mysql -e "$CREATE_DB_QUERY"
+
+
 # # EXPORT DB
 # sudo mysqldump Library > data-dump.sql
 

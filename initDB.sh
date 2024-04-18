01bc487285cc2e1aa8002e4de7fd1210fb545c89
Himanshu Anand <93127466+ImHimanshuAnand@users.noreply.github.com>
2024-04-18 15:33:31 +0000

    Insert data and Print it onto Terminal's Output

--- initDB.sh
+++ initDB.sh
@@ -2,7 +2,7 @@
 MYSQL_USER="root"
 MYSQL_PASSWORD="hv7460"
 MYSQL_HOST="localhost"
-MYSQL_DATABASE="Lib6"
+MYSQL_DATABASE="Lib11"
 
 # MySQL command to create database
 CREATE_DB_QUERY="CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
@@ -30,8 +30,22 @@ CREATE TABLE IF NOT EXISTS $MYSQL_DATABASE.Users (
 sudo mysql -D$MYSQL_DATABASE -e "$CREATE_TABLE_QUERY_USERS"
 
 # SQL keywords are case-insensitive but not Table,Columns,Key Names,etc
-sudo mysql -e "USE Lib6; SELECT * FROM Users;"
+sudo mysql -e "USE $MYSQL_DATABASE; SELECT * FROM Users;"
+# Todo: not printing tables to script's output even after 4-5 attempts
 sudo mysql -t -e "USE $MYSQL_DATABASE; SELECT Email FROM Users;"
 
 # To Check Table Schema, DESC <TableName> or DESCRIBE <TableName>
 sudo mysql -e "USE $MYSQL_DATABASE; DESCRIBE Users;"
+
+# MySQL command to insert data into tables
+INSERT_DATA_QUERY_USERS="
+INSERT INTO $MYSQL_DATABASE.Users (UserId, StudentID, AdminID, UserType, Email, Password)
+VALUES (1, NULL, 101, 'Admin', 'admin@example.com', 'admin_password'),
+       (2, 1001, NULL, 'Student', 'student@example.com', 'student_password');"
+       
+sudo mysql -D$MYSQL_DATABASE -e "$INSERT_DATA_QUERY_USERS"
+
+# echo "select value1, value2 from table" | mysql -uUSER -pPASS | column -t
+# echo "select * from table" | mysql -uUSER -pPASS | column -t
+echo "USE $MYSQL_DATABASE; select * from Users" | sudo mysql | column -t
+# Successfully Prints Table data onto terminal output after 3-4 attempts
